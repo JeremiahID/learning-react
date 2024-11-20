1a9c925379e75a1b31d2a68afaffcceee98695e5
JeremiahID <jeremiahefe.je@gmail.com>
2024-11-20 17:55:48 +0100

    error resolved

--- src/contact.jsx
+++ src/contact.jsx
@@ -6,7 +6,7 @@ function Card(props){
     return (
         <div className="card">
             <h2>{props.name}</h2>
-            <Images />
+            <Images img = {props.img}/>
             <p>{props.tel}</p>
             <p>{props.email}</p>
         </div>
